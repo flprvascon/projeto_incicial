9b8b5dc08e5fd78533d4c14edd03d5b01ed7d825
Filipe <flprvascon@gmail.com>
2024-12-02 20:29:16 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 31
+nome = "Filipe"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1, 2, 3, 4, 5]
+
+# Métodos
+lista.reverse()  # Inverte a lista no lugar
+print(lista)  # Certifique-se de exibir a lista invertida
+
+
